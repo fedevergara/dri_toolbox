1dbef8aac024d57e10c3b9121f40a082d9b8bec6
Federico Vergara <fedevego15@gmail.com>
2023-11-01 10:02:35 -0500

    Fixed Dash app integration

--- app.py
+++ app.py
@@ -1,28 +1,29 @@
 from utils.listas import tipos_documento, paises, vinculos, unidades, sedes
 from flask import Flask, request, render_template
 from utils.actualizacion_eventos import actualizar_eventos
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from pymongo import MongoClient, DESCENDING
 from utils.send_email import enviar_correo
-from app_dash import app as dash_app
+from dash import Dash, html, dcc, Output, Input
 from bson.objectid import ObjectId
 from time import time
 import qrcode
 import json
 import os
+import pandas as pd
+import plotly.express as px
 
 db_uri = "mongodb://localhost:27017/"
 db_name = "international"
-collection_name = "events"
-assistants = "assistants"
+events_collection = "events"
+assistants_collection = "assistants"
 
 dbclient = MongoClient(db_uri)
 db = dbclient[db_name]
-collection = db[collection_name]
-collection_assistants = db[assistants]
+events = db[events_collection]
+assistants = db[assistants_collection]
 
 # Create Flask instance
-app = Flask(__name__)
+server = Flask(__name__)
 
 registros_eventos = actualizar_eventos()
 
@@ -38,12 +39,12 @@ for evento in registros_eventos:
 # Create a route
 
 
-@app.route("/")
+@server.route("/")
 def index():
     return render_template("index.html")
 
 
-@app.route("/registro_movilidad", methods=["GET", "POST"])
+@server.route("/registro_movilidad", methods=["GET", "POST"])
 def registro_movilidad():
     record = None
 
@@ -72,17 +73,17 @@ def registro_movilidad():
                            record=record)
 
 
-@app.route('/buscar_correo')
+@server.route('/buscar_correo')
 def buscar_correo():
     email = request.args.get('email')
-    document = collection.find_one({'email': email},
-                                   sort=[('_id', DESCENDING)])
+    document = events.find_one({'email': email},
+                               sort=[('_id', DESCENDING)])
 
     response = json.dumps(document, default=str, ensure_ascii=False)
     return response
 
 
-@app.route('/registro_eventos', methods=['GET', 'POST'])
+@server.route('/registro_eventos', methods=['GET', 'POST'])
 def registro_eventos():
     record = None
     error = None
@@ -94,7 +95,7 @@ def registro_eventos():
         documento_identidad = request.form['documento']
 
         # Añade una verificación para evitar registros duplicados basados en correo y eventos
-        existing_record = collection.find_one({
+        existing_record = events.find_one({
             "email": email,
             "eventos": {"$in": evento},
             "numero_documento_identidad": documento_identidad
@@ -112,7 +113,8 @@ def registro_eventos():
             entidad = request.form['entidad'].strip().title()
             vinculo = request.form['vinculo']
             unidad = request.form['unidad'] if vinculo != "Externo" else "N/A"
-            programa = request.form['programa'].strip().title() if vinculo != "Externo" else "N/A"
+            programa = request.form['programa'].strip(
+            ).title() if vinculo != "Externo" else "N/A"
             sede = request.form['sede'] if vinculo != "Externo" else "N/A"
 
             record = {
@@ -185,7 +187,7 @@ def registro_eventos():
 
         # Insertar el documento en la colección de MongoDB
         record['qr'] = QR
-        collection.insert_one(record)
+        events.insert_one(record)
 
         del record['_id']
         del record['registro']
@@ -193,7 +195,8 @@ def registro_eventos():
         try:
             # Enviar correo electrónico con el código QR
             qr_url = PATH + QR
-            enviar_correo(record['email'], f"Registro exitoso - De País en País Centroamérica y el Caribe - {dehydrated_record['email']}", dehydrated_record, qr_url, record["eventos"])
+            enviar_correo(
+                record['email'], f"Registro exitoso - De País en País Centroamérica y el Caribe - {dehydrated_record['email']}", dehydrated_record, qr_url, record["eventos"])
         except Exception as e:
             print("Error al enviar el correo electrónico:", e)
 
@@ -212,7 +215,7 @@ def registro_eventos():
     )
 
 
-@app.route('/registro_asistencia', methods=['GET', 'POST'])
+@server.route('/registro_asistencia', methods=['GET', 'POST'])
 def registro_asistencia():
     registro = None
 
@@ -224,7 +227,8 @@ def registro_asistencia():
             data_evento = data['evento']
             json_text = data_evento.replace("'", "\"")
             evento_json = json.loads(json_text)
-            evento_registrado = evento_json['dia'] + " | " + evento_json['evento']
+            evento_registrado = evento_json['dia'] + \
+                " | " + evento_json['evento']
 
         except Exception as e:
             print("Error al cargar los datos:", e)
@@ -243,7 +247,7 @@ def registro_asistencia():
             # Accede a la cámara y captura información del QR (implementación necesaria)
             # Verifica y almacena el registro en MongoDB
 
-            existing_record = collection_assistants.find_one({
+            existing_record = assistants.find_one({
                 "email": registro['email'],
                 "numero_documento_identidad": registro['numero_documento_identidad'],
                 "nombres": registro['nombres'],
@@ -252,7 +256,7 @@ def registro_asistencia():
             })
 
             if not existing_record:
-                collection_assistants.insert_one(record)
+                assistants.insert_one(record)
                 print("Registro exitoso.")
             else:
                 print("Ya se encuentra registrada la asistencia.")
@@ -269,14 +273,128 @@ def registro_asistencia():
                            eventos=eventos_enviar)
 
 
-@app.route('/DPEP/indicadores_registros')
-def dash():
-    return dash_app.index()
+# app = Dash(server=server, routes_pathname_prefix="/dash/")
+# app.layout = create_app()
+
+# Aplicación Dash
+
+def actualizacion_eventos(events):
+    datos_events = list(events.find())
+    return datos_events
+
+
+def crear_dataframe(datos_events):
+
+    # Inicializa listas para construir el DataFrame
+    asistencias = []
+    vinculos = []
+    sedes = []
+
+    # Itera sobre los documentos para obtener asistencias únicas a cada evento
+    for documento in datos_events:
+        eventos = documento['eventos']
+        email = documento['email']
+        vinculo = documento['vinculo']
+        sede = documento['sede']
+
+        for evento in eventos:
+            asistencias.append({'email': email, 'dia': evento.split(
+                "|")[0].strip(), 'evento': evento.split("|")[1].strip()})
+            vinculos.append(vinculo)
+            sedes.append(sede)
+
+    # Crea un DataFrame con las asistencias
+    df = pd.DataFrame(asistencias)
+
+    # Agrega las columnas 'vinculo' y 'sede' al DataFrame
+    df['vinculo'] = vinculos
+    df['sede'] = sedes
+
+    # Calcula el conteo de eventos
+    conteo_eventos = df['evento'].value_counts()
+    conteo_eventos_dict = conteo_eventos.to_dict()
+    df['conteo_evento'] = df['evento'].map(conteo_eventos_dict)
+
+    return df
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = Dash(server=server, routes_pathname_prefix="/dash/")
+df = crear_dataframe(actualizacion_eventos(events))
+
+app.layout = html.Div([
+    html.H1("Conteo de registros - eventos de De País en País Centroamérica y el Caribe",
+            style={"textAlign": "center"}),
+    html.Hr(),
+    html.Div(id="bar-div", children=[]),
+    html.Hr(),
+    html.P("Eventos:"),
+    html.Div(html.Div([
+        dcc.Dropdown(id='dia-lista', clearable=False,
+                     value="Viernes 3 de noviembre",
+                     options=[{'label': x, 'value': x} for x in
+                              df["dia"].unique()]),
+    ]), className="row"),
+
+
+    html.Div(id="output-div", children=[]),
+])
+
+
+@app.callback([
+    Output(component_id="bar-div", component_property="children"),
+    Output(component_id="output-div", component_property="children")],
+    Input(component_id="dia-lista", component_property="value"),
+)
+def make_graphs(dia_escogido):
+    df = crear_dataframe(actualizacion_eventos(events))
+
+    # BAR CHART
+    df_bar = df['vinculo'].value_counts().reset_index()
+    df_bar.columns = ['vinculo', 'total_asistentes']
+    total_inscritos = df_bar['total_asistentes'].sum()
+    total_inscritos_df = pd.DataFrame(
+        {'vinculo': ['Total de Inscritos'], 'total_asistentes': [total_inscritos]})
+    df_bar = pd.concat([df_bar, total_inscritos_df])
+
+    # Crear el gráfico de barras
+    fig_bar = px.bar(df_bar, x="total_asistentes", y="vinculo",
+                     color="vinculo", text="total_asistentes")
+    fig_bar.update_layout(
+        title="Total de Asistentes por Vínculo",
+        xaxis_title="Número de Asistentes",
+        yaxis_title="Vínculo",
+    )
+
+    fig_bar.update_traces(marker=dict(line=dict(width=1)))
+    fig_bar.update_xaxes(dtick=1)
+
+    # HISTOGRAM
+    df_hist_ = df[df["dia"] == dia_escogido]
+    df_hist = df_hist_.pivot_table(values='email', index=[
+        'dia', 'evento', 'vinculo'], aggfunc='count').reset_index().sort_values(by='email', ascending=False)
+    fig_hist = px.bar(df_hist, x='email', y="evento",
+                      color="vinculo", text="email")
+    fig_hist.update_xaxes(dtick=1)
+    fig_hist.update_layout(
+        title="Asistentes por Vínculo a eventos del día {}".format(
+            dia_escogido),
+        xaxis_title="Número de Asistentes",
+        yaxis_title="Vínculo",
+    )
+
+    return [
+        html.Div([
+            html.Div([dcc.Graph(figure=fig_bar)], className="twelve columns"),
+        ], className="row"),
+        html.Div([
+            html.Div([dcc.Graph(figure=fig_hist)], className="twelve columns"),
+        ], className="row")]
 
 
 # Errors handlers
 # Invalid URL
-@app.errorhandler(404)
+@server.errorhandler(404)
 def page_not_found(e):
     print(e)
     return render_template("404.html"), 404
@@ -284,12 +402,11 @@ def page_not_found(e):
 # Internal server error
 
 
-@app.errorhandler(500)
+@server.errorhandler(500)
 def internal_server_error(e):
     print(e)
     return render_template("505.html"), 500
 
 
 if __name__ == "__main__":
-    application = DispatcherMiddleware(app, {'/DPEP/indicadores_registros': dash_app.server})
     app.run(host="0.0.0.0", port=8001, debug=True)
